1b12c88960a01140b64c2f1ecc35cc082c826bd9
Rahul Sah <rahulkrsah1999@gamil.com>
2022-09-29 12:46:56 +0530

    user added

--- models/user.js
+++ models/user.js
@@ -3,5 +3,13 @@ const mongooes = require('mongoose')
 const Schema  = mongoose.Schema;
 
 const UserSchema = new Schema({
-    
+    name : {
+        firstname : {
+            type : String,
+            required : true
+        },
+        lastname : {
+            type: String
+        }
+    }
 })
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./db')
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Welcome to port 3000</h>')
+    res.send('<h1>Welcome to port 3000 </h>')
 })
 app.listen('3000',()=>{
     console.log('server running on port 3000')
